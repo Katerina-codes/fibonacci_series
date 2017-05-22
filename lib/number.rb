1c54943963945a4e005d62428d98c91772c0cd0b
Katerina <katerinaldg@gmail.com>
2017-05-22 16:16:23 +0100

    Made code more generic

--- lib/number.rb
+++ lib/number.rb
@@ -1,15 +1,9 @@
 class Number
 
   def next_number(array)
-    if array == [0, 1]
-      1
-    elsif array == [0, 1, 1]
-      2
-    elsif array == [0, 1, 1, 2]
-      3
-    else
-      5
-    end
+    next_number = array[-1] + array[-2]
+    array.push(next_number)
+    next_number
   end
 
 end
