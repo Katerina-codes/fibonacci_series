65855e0633fe2f5641b430e16e1acf82e7ba4642
Katerina <katerinaldg@gmail.com>
2017-05-22 15:19:37 +0100

    Returns 2 if previous numbers are 1, 1

--- lib/number.rb
+++ lib/number.rb
@@ -1,7 +1,11 @@
 class Number
 
   def next_number(array)
-    1
+    if array == [0, 1]
+      1
+    else
+      2
+    end
   end
 
 end
--- spec/number_spec.rb
+++ spec/number_spec.rb
@@ -2,9 +2,14 @@ require 'number'
 
 describe Number do
 
-  it "returns 1 if first 2 numbers are 0, 1" do
+  it "returns 1 if previous 2 numbers are 0, 1" do
     number = Number.new
-    expect(number.next_number([0,1])).to eq(1)
+    expect(number.next_number([0, 1])).to eq(1)
+  end
+
+  it "returns 2 if previous 2 numbers are 1,1" do
+    number = Number.new
+    expect(number.next_number([1, 1])).to eq(2)
   end
 
 end
